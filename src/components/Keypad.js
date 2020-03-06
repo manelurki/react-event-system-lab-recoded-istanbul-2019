897997fe68c6329495cf671b32c6fdbd73ff3b6f
manel turki <menelturki@gmail.com>
2020-03-06 21:16:45 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
-// Code Keypad Component Here
+import React,{Component} from 'react'
+class Keypad extends Component {
+    constructor(){
+        super()
+    }
+        keyUpHandler = () =>{
+console.log('Entering password...');
+    }
+    render(){
+return (
+<input type="password" onKeyUp={this.keyUpHandler}/>
+);
+    }
+
+
+
+
+
+
+}
